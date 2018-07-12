ef3a98c7b6a86df0a99aea554192a373af55805a
Administrator <842404548@qq.com>
2018-07-12 11:38:44 +0800

    config-server

---
+++ configserver/src/main/java/com/mistra/configserver/ConfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.mistra.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+//开启ConfigServer功能
+@EnableConfigServer
+public class ConfigserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserverApplication.class, args);
+    }
+}
