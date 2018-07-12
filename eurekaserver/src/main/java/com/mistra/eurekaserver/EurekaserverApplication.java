409f769c492e1b817ae13477d73e828529ee624a
Administrator <842404548@qq.com>
2018-07-12 11:23:37 +0800

    eureka-server

--- eurekaserver/src/main/java/com/mistra/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/mistra/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,11 @@ package com.mistra.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//开启EurekaServer功能
+@EnableEurekaServer
 public class EurekaserverApplication {
 
     public static void main(String[] args) {
