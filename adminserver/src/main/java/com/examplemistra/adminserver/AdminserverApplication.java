97db589012c77006f1631c864f3bd35fdaa82a30
MistraR <mistra3030000>
2018-07-15 23:32:55 +0800

    adminserver

--- adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
+++ adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
@@ -1,14 +1,17 @@
 package com.examplemistra.adminserver;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -21,34 +24,75 @@ public class AdminserverApplication {
     }
 
     // tag::configuration-spring-security[]
-    @Configuration
-    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
+    //@Configuration
+    //public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+       // @Override
+       // protected void configure(HttpSecurity http) throws Exception {
             // Page with login form is served as /login.html and does a POST on /login
-            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+           // http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
             // The UI does a POST on /logout on logout
-            http.logout().logoutUrl("/logout");
+           // http.logout().logoutUrl("/logout");
             // The ui currently doesn't support csrf
-            http.csrf().disable();
+           // http.csrf().disable();
 
             // Requests for the login page and the static assets are allowed
-            http.authorizeRequests()
-                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
-                    .permitAll();
+           // http.authorizeRequests()
+                    //.antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                    //.permitAll();
             // ... and any other request needs to be authorized
-            http.authorizeRequests().antMatchers("/**").authenticated();
+           // http.authorizeRequests().antMatchers("/**").authenticated();
 
             // Enable so that the clients can authenticate via HTTP basic for registering
-            http.httpBasic();
-        }
-    }
+          //  http.httpBasic();
+      //  }
+    //}
 
-    @Autowired
+   /* @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
                 .withUser("admin").password("123456").roles("USER");
-    }
+    }*/
     // end::configuration-spring-security[]
+
+    @Profile("insecure")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()//
+                    .and().csrf().disable();
+        }
+    }
+
+    @Profile("secure")
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
+                    .and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
+            // @formatter:on
+        }
+    }
 }
