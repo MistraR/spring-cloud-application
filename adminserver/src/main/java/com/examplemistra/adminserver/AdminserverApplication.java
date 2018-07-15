a28a2c5b9675da602020faa331a60768e9adcb7f
MistraR <mistra3030000>
2018-07-15 15:29:25 +0800

    adminserver

---
+++ adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
@@ -0,0 +1,12 @@
+package com.examplemistra.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminserverApplication.class, args);
+    }
+}
--- routeservice/src/main/java/com/mistra/routeservice/RouteserviceApplication.java
+++ routeservice/src/main/java/com/mistra/routeservice/RouteserviceApplication.java
@@ -2,8 +2,11 @@ package com.mistra.routeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+//开启Zuul
+@EnableZuulProxy
 public class RouteserviceApplication {
 
     public static void main(String[] args) {
