099dd09bf60060253312c80a7de82fc76dbd3f77
MistraR <mistra3030000>
2018-07-15 22:45:36 +0800

    adminserver

--- adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
+++ adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
@@ -1,12 +1,54 @@
 package com.examplemistra.adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
+@EnableEurekaClient
+//开启AdminServer功能
+@EnableAdminServer
 public class AdminserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminserverApplication.class, args);
     }
+
+    // tag::configuration-spring-security[]
+    @Configuration
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // Page with login form is served as /login.html and does a POST on /login
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            // The UI does a POST on /logout on logout
+            http.logout().logoutUrl("/logout");
+            // The ui currently doesn't support csrf
+            http.csrf().disable();
+
+            // Requests for the login page and the static assets are allowed
+            http.authorizeRequests()
+                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                    .permitAll();
+            // ... and any other request needs to be authorized
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            // Enable so that the clients can authenticate via HTTP basic for registering
+            http.httpBasic();
+        }
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("123456").roles("USER");
+    }
+    // end::configuration-spring-security[]
 }
