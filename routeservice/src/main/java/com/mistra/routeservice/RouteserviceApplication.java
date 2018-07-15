5c4cc4b59f90d747fa0245e7e630db9ef0183c50
MistraR <mistra3030000>
2018-07-15 14:54:59 +0800

    routeservice

---
+++ routeservice/src/main/java/com/mistra/routeservice/RouteserviceApplication.java
@@ -0,0 +1,12 @@
+package com.mistra.routeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteserviceApplication.class, args);
+    }
+}
---
+++ routeservice/src/test/java/com/mistra/routeservice/RouteserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mistra.routeservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RouteserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
