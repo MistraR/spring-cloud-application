fb9a29666dfc48c3e1f884b195a101e4012255b9
wangrui4 <842404548@qq.com>
2018-09-20 14:30:22 +0800

    Hystrix

--- uaaservice/src/main/java/com/mistra/uaaservice/UaaserviceApplication.java
+++ uaaservice/src/main/java/com/mistra/uaaservice/UaaserviceApplication.java
@@ -3,11 +3,13 @@ package com.mistra.uaaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrixDashboard//http://localhost:8002/hystrix
 public class UaaserviceApplication {
 
     public static void main(String[] args) {
