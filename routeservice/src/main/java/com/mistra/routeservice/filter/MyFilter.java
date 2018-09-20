f3b3c10588b73c81d48126332396e3653877bbee
wangrui4 <842404548@qq.com>
2018-09-20 14:36:47 +0800

    Zuul  路由过滤器

---
+++ routeservice/src/main/java/com/mistra/routeservice/filter/MyFilter.java
@@ -0,0 +1,69 @@
+package com.mistra.routeservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author: WangRui
+ * Time: 2018/9/19/003
+ * Describe: Zuul 过滤器
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
+     * filterOrder：过滤的顺序
+     * shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+     * run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     * 访问测试：http://localhost:8888/api-feign/hi?name=forezp||http://localhost:8888/api-feign/hi?name=forezp&token=22
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("Token");
+        if (accessToken == null) {
+            log.warn("Token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("Token is empty");
+            } catch (Exception e) {
+            }
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
