01f56514c9ee77e9d8888023dd4bddde3950e091
wangrui4 <842404548@qq.com>
2018-09-20 20:01:07 +0800

    pom

--- adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
+++ adminserver/src/main/java/com/examplemistra/adminserver/AdminserverApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,49 +14,46 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 
 @SpringBootApplication
 @EnableEurekaClient
-//开启AdminServer功能
+@EnableDiscoveryClient
 @EnableAdminServer
 public class AdminserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminserverApplication.class, args);
     }
-    @Profile("insecure")
+
+    //@Profile("insecure")
     @Configuration
     public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()//
+            http.authorizeRequests().anyRequest().permitAll()
                     .and().csrf().disable();
         }
     }
 
-    @Profile("secure")
-    @Configuration
-    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-        private final String adminContextPath;
-
-        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
-            this.adminContextPath = adminServerProperties.getContextPath();
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-            successHandler.setTargetUrlParameter("redirectTo");
-
-            http.authorizeRequests()
-                    .antMatchers(adminContextPath + "/assets/**").permitAll()
-                    .antMatchers(adminContextPath + "/login").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-                    .logout().logoutUrl(adminContextPath + "/logout").and()
-                    .httpBasic().and()
-                    .csrf().disable();
-            // @formatter:on
-        }
-    }
+//    @Profile("secure")
+//    @Configuration
+//    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+//        private final String adminContextPath;
+//        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+//            this.adminContextPath = adminServerProperties.getContextPath();
+//        }
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+//            successHandler.setTargetUrlParameter("redirectTo");
+//            http.authorizeRequests()
+//                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+//                    .antMatchers(adminContextPath + "/login").permitAll()
+//                    .anyRequest().authenticated()
+//                    .and()
+//                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+//                    .logout().logoutUrl(adminContextPath + "/logout").and()
+//                    .httpBasic().and()
+//                    .csrf().disable();
+//        }
+//    }
 
 }
